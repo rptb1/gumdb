9a764ef0f1560961b8e6ff6569773957a34ba587
Richard Brooksby <rb@ravenbrook.com>
2004-02-18 11:05:32 +0000

    Parsing headers, recipients, date, and time fields into separate tables and columns.
    Copied from Perforce
     Change: 78682

--- import.py
+++ import.py
@@ -7,14 +7,65 @@ import sys
 from pyPgSQL import PgSQL
 db = PgSQL
 from db_table_dict import db_table_dict
+import email
+import time
+
+error = "Error"
 
 connection = db.connect('::mail')
 
 messages = db_table_dict(connection,
                          "messages",
-                         ("message_id", "serial"),
-                         ("message_text", "text"))
+                         ("message_id", "text"),
+                         [("envelope_from", "text"),
+                          ("date", "date"),
+                          ("time", "time")])
+
+message_texts = db_table_dict(connection,
+                              "message_texts",
+                              ("message_id", "text"),
+                              ("message_text", "text"))
+
+message_headers = db_table_dict(connection,
+                                "message_headers",
+                                [("message_id", "text"),
+                                 ("header_index", "int")],
+                                [("header_name", "text"),
+                                 ("header_value", "text")])
+
+message_recipients = db_table_dict(connection,
+                                   "message_recipients",
+                                   [("message_id", "text"),
+                                    ("recipient_index", "int")],
+                                   [("recipient_type", "text"),
+                                    ("recipient_name", "text"),
+                                    ("recipient_address", "text")])
 
 message_text = sys.stdin.read()
 
-messages.append(message_text)
+message = email.message_from_string(message_text)
+
+# Need to decide what to do if any of these fields don't exist or can't
+# be parsed.
+message_id = message["message-id"]
+envelope_from = message.get_unixfrom()
+stamp = email.Utils.mktime_tz(email.Utils.parsedate_tz(message["date"]))
+iso_date = time.strftime("%Y-%m-%d", time.gmtime(stamp))
+iso_time = time.strftime("%H:%M:%S", time.gmtime(stamp))
+
+if message_id in messages:
+    raise error, "Duplicate message with id %s" % repr(message_id)
+
+messages[message_id] = (envelope_from, iso_date, iso_time)
+message_texts[message_id] = message_text
+
+index = 0
+for (name, value) in message.items():
+    message_headers[(message_id, index)] = (name.lower(), value)
+    index += 1
+
+index = 0
+for type in ['to', 'cc', 'bcc']:
+    for (name, address) in email.Utils.getaddresses(message.get_all(type, [])):
+        message_recipients[(message_id, index)] = (type, name, address)
+        index += 1
--- tables.sql
+++ tables.sql
@@ -2,7 +2,36 @@
 --
 -- $Id$
 
+DROP TABLE messages CASCADE;
+DROP TABLE message_texts CASCADE;
+DROP TABLE message_headers CASCADE;
+DROP TABLE message_recipients CASCADE;
+
 CREATE TABLE messages (
-    message_id serial PRIMARY KEY,
+    message_id text PRIMARY KEY,
+    envelope_from text,
+    "date" date,
+    "time" time
+);
+
+CREATE TABLE message_texts (
+    message_id text PRIMARY KEY REFERENCES messages,
     message_text text
+);
+
+CREATE TABLE message_headers (
+    message_id text REFERENCES messages,
+    header_index int NOT NULL,
+    PRIMARY KEY (message_id, header_index),
+    header_name text NOT NULL,
+    header_value text NOT NULL
+);
+
+CREATE TABLE message_recipients (
+    message_id text REFERENCES messages,
+    recipient_index int NOT NULL,
+    PRIMARY KEY (message_id, recipient_index),
+    recipient_type text NOT NULL, -- 'to', 'cc', or 'bcc'
+    recipient_name text NOT NULL,
+    recipient_address text NOT NULL
 );
